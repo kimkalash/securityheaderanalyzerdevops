d1b95fccc23b2ac2819f73b7a48d495b7445c50a
kimkalash <Sanurussia@gmail.com>
2025-05-03 14:12:48 +0000

    dockerized app and confirmed container runs

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Security Headers Analyzer is live"}
+# instruct server when entrypoint is run
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
